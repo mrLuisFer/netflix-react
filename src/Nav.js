081009bef38d1075a588b8be1bf8e53c2d19cd6a
Luis_Alvarez <lolesuncrak@gmail.com>
2020-08-08 23:30:00 -0500

    navbar finished

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './nav.css';
+import { useState } from 'react';
 
 const Nav = () => {
-	return <div></div>;
+	const [show, handleShow] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 250) {
+				handleShow(true);
+			} else handleShow(false);
+		});
+
+		return () => {
+			window.removeEventListener('scroll');
+		};
+	}, []);
+
+	return (
+		<div className={`nav ${show && 'nav_black'}`}>
+			<img
+				className="nav_logo"
+				src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+				alt="Netflix Logo"
+			/>
+
+			<img
+				class="nav_avatar"
+				src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+				alt="Netflix Logo"
+			></img>
+		</div>
+	);
 };
 
 export default Nav;
